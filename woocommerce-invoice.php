496dc6794faa6ec6e2ec94a7116ccb0d99490ad9
Ahmadreza <ahmadreza.work.99@gmail.com>
2024-08-17 13:26:05 +0330

    Add plugin main files

---
+++ woocommerce-invoice.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Woocommerce Invoice
+ *
+ * @package           WoocommerceInvoice
+ * @author            Ahmadreza Ebrahimi
+ * @copyright         2024 Ahmadreza Ebrahimi
+ * @license           GPL-3.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Woocommerce Invoice
+ * Plugin URI:        https://wordpress.org/plugin/woocommerce-invoice
+ * Description:       Create purchase invoices for customers who have purchased from you.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4
+ * Author:            Ahmadreza Ebrahimi
+ * Author URI:        https://github.com/ahmadreza-log/
+ * Text Domain:       wc-invoice
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.txt
+ */

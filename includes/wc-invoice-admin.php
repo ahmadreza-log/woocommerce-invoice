371643604567d6d69d2245e529f24d13a73d8e06
Ahmadreza <ahmadreza.work.99@gmail.com>
2024-08-17 18:31:27 +0330

    Working On Settings Page

--- includes/wc-invoice-admin.php
+++ includes/wc-invoice-admin.php
@@ -22,12 +22,12 @@ if ( ! class_exists( 'WC_Invoice_Admin' ) ) {
 
 			$this->tab     = 'invoice';
 			$this->title   = __( 'Invoice', 'wc-invoice' );
-			$this->section = ! is_null( $current_section ) ? $current_section : '';
+			$this->section = ! is_null( $current_section ) ? $current_section : 'general';
 
 			add_filter( 'woocommerce_settings_tabs_array', [ $this, 'tabs' ], 30 );
-			add_action( "woocommerce_sections_{$this->tab}", [ $this, 'sections' ] );
-			add_action( "woocommerce_settings_{$this->tab}", [ $this, 'output' ] );
-			add_action( "woocommerce_settings_save_{$this->tab}", [ $this, 'save' ] );
+			add_action( 'woocommerce_sections_' . $this->tab, [ $this, 'sections' ] );
+			add_action( 'woocommerce_settings_' . $this->tab, [ $this, 'output' ] );
+			add_action( 'woocommerce_settings_save_' . $this->tab, [ $this, 'save' ] );
 		}
 
 		public function tabs( $tabs ): array {
@@ -71,7 +71,51 @@ if ( ! class_exists( 'WC_Invoice_Admin' ) ) {
 		public function options(): array {
 			$settings = [];
 
-			return apply_filters( "woocommerce_get_settings_{$this->tab}", $settings );
+			$settings = [
+				'title'                  => [
+					'title' => __( 'General Options', 'wc-invoice' ),
+					'type'  => 'title',
+					'desc'  => __( 'Specify what elements you want to be shown in the invoice.', 'wc-invoice' ),
+				],
+				'show-name'             => [
+					'title'       => __( 'Store Name', 'wc-invoice' ),
+					'type'        => 'text',
+					'desc'        => __( 'The name of your store that will be shown on the invoice. (If you leave it blank, it will use the name of the site by default)', 'wc-invoice' ),
+					'desc_tip'    => true,
+					'placeholder' => get_bloginfo( 'name' ),
+				],
+				'show-logo'                   => [
+					'title' => __( 'Logo', 'wc-invoice' ),
+					'type'  => 'checkbox',
+					'desc'  => __( 'Show the store logo on the invoice', 'wc-invoice' ),
+				],
+				'show-address'     => [
+					'title'   => __( 'Address', 'wc-invoice' ),
+					'desc'    => __( 'Show the store address on the invoice', 'wc-invoice' ),
+					'default' => 'yes',
+					'type'    => 'checkbox',
+				],
+				'show-products-discount'  => [
+					'title'   => __( 'Products Discount', 'wc-invoice' ),
+					'desc'    => __( 'Display the discount applied to the list products.', 'wc-invoice' ),
+					'default' => 'no',
+					'type'    => 'checkbox',
+				],
+				'show-products-thumbnail' => [
+					'title'    => __( 'Products Thumbnail', 'wc-invoice' ),
+					'desc' => __( 'Display the thumbnail of product applied to the list products.', 'wc-invoice' ),
+					'default'  => 'no',
+					'type'     => 'checkbox',
+				],
+			];
+
+			$settings[ 'end' ] = [ 'type' => 'sectionend' ];
+
+			foreach ( $settings as $key => $value ) {
+				$settings[ $key ][ 'id' ] = 'wc_invoice_' . $this->section . '_setting_' . $key;
+			}
+
+			return apply_filters( 'woocommerce_get_settings_' . $this->tab, $settings );
 		}
 
 		public function output() {
